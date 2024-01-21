069e1325e9aa1fd924999460f547e5f5802c6d07
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-01-21 14:11:38 +0700

    try changing user and exact environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import './App.css'
-// import Webcam from "react-webcam";
-// import CameraDetection from "./components/CameraDetection";
-import CameraComponent from "./components/CameraComponent";
+import Webcam from "react-webcam";
+import { useEffect, useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const videoConstraints: boolean | MediaTrackConstraints = {
-  //   facingMode: 'user',
-  // }
+  const [choosenCamera, setChoosenCamera] = useState<'front' | 'back'>('front');
+  let videoConstraints: boolean | MediaTrackConstraints = {
+    facingMode: 'user',
+  }
+
+  useEffect(() => {
+    if (choosenCamera === 'back') {
+      videoConstraints = {
+        facingMode: { exact: 'environment' },
+      };
+    } else {
+      videoConstraints = {
+        facingMode: 'user',
+      };
+    }
+  }, [choosenCamera]);
 
   return (
     <>
       <div>
         <p className="text-red-700">Hello bang</p>
       </div>
-      {/* <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center">
         <Webcam
           videoConstraints={videoConstraints}
           mirrored
         />
-      </div> */}
-      <CameraComponent />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (choosenCamera === 'back') {
+              setChoosenCamera('front');
+            } else {
+              setChoosenCamera('back');
+            }
+          }}
+        >Ganti kamera</button>
+      </div>
     </>
   )
 }
